70dcddc0733da83cea92fbd5675802c7563ebad8
tgibson22 <tim12069@gmail.com>
2020-10-01 10:16:34 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <UrlShortener msg="Fancy Url Shortener"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import UrlShortener from './components/UrlShortener.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    UrlShortener
   }
 });
 </script>
